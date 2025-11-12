81e979734022ec6d7136c59713ae4f1cd4cb7964
Bruno Giuliano Vapore <vapore.bg@gmail.com>
2025-11-12 18:23:17 -0300

    Update comment

--- src/main/java/Dao/GenericDAO.java
+++ src/main/java/Dao/GenericDAO.java
@@ -2,7 +2,7 @@ package Dao;
 
 public interface GenericDAO<T> {
     // Esta es una interfaz genérica que define métodos comunes para trabajar con cualquier entidad.
-    // Sirve como base para evitar repetir código en distintas clases DAO (como PersonaDAO o LegajoDAO).
+    // Sirve como base para evitar repetir código en distintas clases DAO (como EmpleadoDAO o LegajoDAO).
 
 
 }
