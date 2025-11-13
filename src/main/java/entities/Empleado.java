409904cd3dbcbf11f1b57100341c077316944815
BrunoGVapore <vapore.bg@gmail.com>
2025-11-12 23:13:32 -0300

    Actualizado entities

--- src/main/java/entities/Empleado.java
+++ src/main/java/entities/Empleado.java
@@ -1,5 +1,7 @@
 package entities;
 
+import java.util.Date;
+
 /**
  * Entidad que representa un empleado en el sistema.
  * Hereda de Base para obtener id y eliminado.
@@ -8,20 +10,124 @@ package entities;
  * Asociación unidireccional 1 a 1. 
  * Empleado -> Legajo
  *
- * Tabla BD: ....
+ * Tabla BD: empleado
  * Campos:
- * - id: INT AUTO_INCREMENT PRIMARY KEY (heredado de Base)
- * - ...
- * - ....
- * - eliminado: BOOLEAN DEFAULT FALSE (heredado de Base)
+ * - id: int AUTO_INCREMENT PRIMARY KEY UNIQUE NOT NULL (heredado de Base)
+ * - eliminado: BOOLEAN DEFAULT FALSE NOT NULL (heredado de Base)
+ * - nombre: String NOT NULL
+ * - apellido: String NOT NULL
+ * - dni: String UNIQUE NOT NULL
+ * - email: String
+ * - fecha_ingreso: Date
+ * - area: String
  */
 public class Empleado extends Base {
     
-    // Atributos
- 
-    // Constructor
+    // Atributos propios
+    private String nombre; // obligatorio
+    private String apellido; // obligatorio
+    private String dni; // obligatorio, único e irrepetible
+    private String email; // opcional, único
+    private Date fechaIngreso; // Inicio de la relación laboral
+    private String area;
     
-    // Métodos
+    private Legajo legajo; // Asociación 1:1 unidireccional con Legajo
     
-  
+    // Constructores
+    
+    public Empleado(String nombre, String apellido, String dni, String email, Date fechaIngreso, String area, Legajo legajo) { // Completo para el DAO y trabajar desde la BD
+        super(); // Llama al constructor de Base que contiene id y eliminado por defecto
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.dni = dni;
+        this.email = email;
+        this.fechaIngreso = fechaIngreso;
+        this.area = area;
+        this.legajo = legajo;
+    }
+
+    public Empleado() { // Explícito, para el Menú y su construcción a través de setters
+        super(); 
+    }
+    
+    
+    // Getters y Setters
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Date getFechaIngreso() {
+        return fechaIngreso;
+    }
+
+    public void setFecha_ingreso(Date fechaIngreso) {
+        this.fechaIngreso = fechaIngreso;
+    }
+
+    public String getArea() {
+        return area;
+    }
+
+    public void setArea(String area) {
+        this.area = area;
+    }
+
+    public Legajo getLegajo() {
+        return legajo;
+    }
+
+    public void setLegajo(Legajo legajo) {
+        this.legajo = legajo;
+    }
+
+    @Override
+    public void eliminarRegistro() { // Borrado lógico
+        super.setEliminado(true); 
+    }
+
+    @Override
+    public String toString() {
+        return "Empleado (ID: " + getId() + ")" +
+               "\nNombre: " + getNombre() + 
+               "\nApellido: " + getApellido() + 
+               "\nDNI: " + getDni() + 
+               "\nEmail: " + getEmail() + 
+               "\nFecha de Ingreso: " + getFechaIngreso() + 
+               "\nArea: " + getArea() +
+               "\nEliminado: " + isEliminado() + 
+               "\nLegajo: (" + getLegajo() + ").\n";
+    }
+    
+    
+    
+    
 }
--- src/main/java/entities/Legajo.java
+++ src/main/java/entities/Legajo.java
@@ -1,5 +1,9 @@
 package entities;
 
+import java.util.Date;
+
+
+
 /**
  * Entidad que representa un legajo en el sistema.
  * Hereda de Base para obtener id y eliminado.
@@ -8,20 +12,97 @@ package entities;
  * Asociación unidireccional 1 a 1. 
  * Empleado -> Legajo
  *
- * Tabla BD: ....
+ * Tabla BD: legajo
  * Campos:
- * - id: INT AUTO_INCREMENT PRIMARY KEY (heredado de Base)
- * - ...
- * - ....
- * - eliminado: BOOLEAN DEFAULT FALSE (heredado de Base)
+ * - id: int AUTO_INCREMENT PRIMARY KEY UNIQUE NOT NULL (heredado de Base)
+ * - eliminado: BOOLEAN DEFAULT FALSE NOT NULL (heredado de Base)
+ * - nro_legajo: String NOT NULL 
+ * - categoria: String
+ * - estado: enum('ACTIVO, INCACTIVO') NOT NULL
+ * - feha_alta: date
+ * - observaciones: String
  */
 public class Legajo extends Base {
     
-    // Atributos
- 
+    // Atributos propios
+    private String numeroLegajo;
+    private String categoria;
+    private EstadoLegajo estado; // Enum (estado contractual activo o inactivo)
+    private Date fechaAlta; // Creación administrativa en el sistema
+    private String observaciones;
+    
     // Constructor
     
-    // Métodos
+    public Legajo(String numeroLegajo, String categoria, EstadoLegajo estado, Date fechaAlta, String observaciones) {
+        super(); // Llama al constructor de Base que contiene id y eliminado por defecto
+        this.numeroLegajo = numeroLegajo;
+        this.categoria = categoria;
+        this.estado = estado;
+        this.fechaAlta = fechaAlta;
+        this.observaciones = observaciones;
+    }
+
+    public Legajo() {
+        super();
+    }
+    
+    
   
+    
+    // Getters y Setters
+
+    public String getNumeroLegajo() {
+        return numeroLegajo;
+    }
+
+    public void setNumeroLegajo(String numeroLegajo) {
+        this.numeroLegajo = numeroLegajo;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public EstadoLegajo getEstado() {
+        return estado;
+    }
+
+    public void setEstado(EstadoLegajo estado) {
+        this.estado = estado;
+    }
+
+    public Date getFechaAlta() {
+        return fechaAlta;
+    }
+
+    public void setFechaAlta(Date fechaAlta) {
+        this.fechaAlta = fechaAlta;
+    }
+
+    public String getObservaciones() {
+        return observaciones;
+    }
+
+    public void setObservaciones(String observaciones) {
+        this.observaciones = observaciones;
+    }
+    
+    // Métodos
+
+    @Override
+    public void eliminarRegistro() {  // Borrado lógico
+        this.setEliminado(true);  // Para el atributo eliminado de la superclase
+        this.setEstado(EstadoLegajo.INACTIVO); // Es necesario también cambiar el estado de legajo por su situación contractual.
+        
+    }
+
+    @Override
+    public String toString() {
+        return "NumeroLegajo= " + getNumeroLegajo() + ", categoria= " + getCategoria() + ", estado= " + getEstado() + ", fechaAlta= " + getFechaAlta() + ", observaciones= " + getObservaciones();
+    }
     
 }
