4e0850cc8306fd1180508072ffe9ec1e45b57e5e
BrunoGVapore <vapore.bg@gmail.com>
2025-11-16 11:09:14 -0300

    Main actualizado

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -4,7 +4,11 @@ package main;
 public class Main {
     
     public static void main(String[] args) {
+        // Instanciamos AppMenu
+        AppMenu app = new AppMenu();
         
+        // Llama al método run que inicia el bucle para mostrar y leer las opciones
+        app.run();
     }
     
 }
